9dd9b44f7b05c40815bafd2a6c08931df4356c32
Felix Boons <felixboons@hotmail.com>
2017-03-28 16:20:52 +0200

    - Several text methods made in MainActivity, to test the functionality of the SQLite DB.

--- BioscoopFilet/app/src/main/java/com/filet/bioscoopfilet/PresentationApplicationLogicLayer/MainActivity.java
+++ BioscoopFilet/app/src/main/java/com/filet/bioscoopfilet/PresentationApplicationLogicLayer/MainActivity.java
@@ -7,9 +7,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.filet.bioscoopfilet.DomainModel.Feedback;
+import com.filet.bioscoopfilet.DomainModel.Review;
+import com.filet.bioscoopfilet.DomainModel.Theater;
+import com.filet.bioscoopfilet.DomainModel.Visitor;
+import com.filet.bioscoopfilet.Persistancy.DAOFactory;
+import com.filet.bioscoopfilet.Persistancy.FeedbackDAO;
+import com.filet.bioscoopfilet.Persistancy.ReviewDAO;
+import com.filet.bioscoopfilet.Persistancy.TheaterDAO;
+import com.filet.bioscoopfilet.Persistancy.VisitorDAO;
 import com.filet.bioscoopfilet.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DAOFactory factory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +28,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        testFeedbackData();
+        testReviewData();
     }
 
-    public void informationButton(View v){
+    public void informationButton(View v) {
         Intent intent = new Intent(this, InformationActivity.class);
         startActivity(intent);
+    }
 
+    public void testFeedbackData() {
+        FeedbackDAO feedbackDAO = factory.createFeedbackDAO();
+        feedbackDAO.insertData(new Feedback(1, 1, "Goeie App!"));
+        feedbackDAO.selectData();
+    }
+
+    public void testReviewData() {
+        ReviewDAO reviewDAO = factory.createReviewDAO();
+        reviewDAO.insertData(new Review(1, 1, 1, 5, "Geweldige film"));
+        reviewDAO.selectData();
+    }
+
+    public void testTheaterData() {
+        TheaterDAO theaterDAO = factory.createTheaterDAO();
+        theaterDAO.insertData(new Theater(1, 1, 150));
+        theaterDAO.selectData();
+    }
+
+    public void testVisitorData() {
+        VisitorDAO visitorDAO = factory.createVisitorDAO();
+        visitorDAO.insertData(new Visitor(1, "Tommy", "Heunks"));
+        visitorDAO.selectData();
     }
 }
