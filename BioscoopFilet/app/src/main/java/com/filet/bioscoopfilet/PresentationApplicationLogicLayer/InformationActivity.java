20d10f150612cf0a87125fcad384fe009bf4526f
Bart int Veld <bveld1@avans.nl>
2017-03-28 13:35:45 +0200

    Layout

---
+++ BioscoopFilet/app/src/main/java/com/filet/bioscoopfilet/PresentationApplicationLogicLayer/InformationActivity.java
@@ -0,0 +1,15 @@
+package com.filet.bioscoopfilet.PresentationApplicationLogicLayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.filet.bioscoopfilet.R;
+
+public class InformationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
---
+++ BioscoopFilet/app/src/main/java/com/filet/bioscoopfilet/PresentationApplicationLogicLayer/MainActivity.java
@@ -0,0 +1,19 @@
+package com.filet.bioscoopfilet.PresentationApplicationLogicLayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.filet.bioscoopfilet.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+}
