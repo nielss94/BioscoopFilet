e8e312f96de152005ee05e7d201299e88a95852d
Niels van Dam <nielsvdam@hotmail.com>
2017-03-28 14:55:48 +0200

    Made logic for SQLliteVisitorDAO

--- BioscoopFilet/app/src/main/java/com/filet/bioscoopfilet/Persistancy/SQLiteVisitorDAO.java
+++ BioscoopFilet/app/src/main/java/com/filet/bioscoopfilet/Persistancy/SQLiteVisitorDAO.java
@@ -1,6 +1,11 @@
 package com.filet.bioscoopfilet.Persistancy;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.util.Log;
 
 import com.filet.bioscoopfilet.DomainModel.Ticket;
 import com.filet.bioscoopfilet.DomainModel.Visitor;
@@ -26,11 +31,46 @@ public class SQLiteVisitorDAO implements VisitorDAO {
     }
     @Override
     public ArrayList<Visitor> selectData() {
+        try{
+            SQLiteDatabase readable = db.getReadableDatabase();
+
+            String query = "SELECT * FROM " + db.getDB_TABLE_VISITOR_NAME();
+            Cursor cursor = readable.rawQuery(query, null);
+
+            cursor.moveToFirst();
+            while(cursor.moveToNext() ) {
+                Visitor v = new Visitor(cursor.getInt(cursor.getColumnIndex(db.getCOLUMN_VISITORID())),
+                        cursor.getString(cursor.getColumnIndex(db.getCOLUMN_VISITOR_FIRSTNAME())),
+                        cursor.getString(cursor.getColumnIndex(db.getCOLUMN_VISITOR_LASTNAME())));
+
+                Log.i(TAG, v.toString());
+                Log.i(TAG, "--------------------------------------------");
+
+                visitors.add(v);
+            }
+
+            db.close();
+        }catch(SQLiteException e)
+        {
+            Log.i(TAG,e.getMessage());
+        }
         return visitors;
     }
 
     @Override
-    public void insertData() {
+    public void insertData(Visitor visitor) {
+        try{
+            SQLiteDatabase writable = db.getWritableDatabase();
 
+            ContentValues values = new ContentValues();
+
+            values.put(db.getCOLUMN_VISITOR_FIRSTNAME(),visitor.getFirstName());
+            values.put(db.getCOLUMN_VISITOR_LASTNAME(),visitor.getLastName());
+
+            writable.insert(db.getDB_TABLE_VISITOR_NAME(), null, values);
+        }catch(SQLiteException e)
+        {
+            Log.i(TAG, e.getMessage());
+        }
     }
 }
