5c9992b76f7ca2ad33f43d23d24d6a36f98c89c8
Niels van Dam <nielsvdam@hotmail.com>
2017-03-30 10:13:13 +0200

    Added filmdetailagendaactivity

---
+++ BioscoopFilet/app/src/main/java/com/filet/bioscoopfilet/PresentationApplicationLogicLayer/FilmDetailAgendaActivity.java
@@ -0,0 +1,15 @@
+package com.filet.bioscoopfilet.PresentationApplicationLogicLayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.filet.bioscoopfilet.R;
+
+public class FilmDetailAgendaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_film_detail_agenda);
+    }
+}
