36fcb9028109be1a0da118a34db7a4a0644568ab
Bart int Veld <bveld1@avans.nl>
2017-04-05 15:48:42 +0200

    else if main

--- BioscoopFilet/app/src/main/java/com/filet/bioscoopfilet/PresentationApplicationLogicLayer/MainActivity.java
+++ BioscoopFilet/app/src/main/java/com/filet/bioscoopfilet/PresentationApplicationLogicLayer/MainActivity.java
@@ -109,10 +109,10 @@ public class MainActivity extends AppCompatActivity implements FilmApiConnector.
         if (Locale.getDefault().toString().equalsIgnoreCase("en_us")) {
             item.setIcon(R.drawable.united_states);
         }
-        if (Locale.getDefault().toString().equalsIgnoreCase("en_gb")) {
+        else if (Locale.getDefault().toString().equalsIgnoreCase("en_gb")) {
             item.setIcon(R.drawable.united_kingdom);
         }
-        if (Locale.getDefault().toString().equalsIgnoreCase("nl_nl")) {
+        else if (Locale.getDefault().toString().equalsIgnoreCase("nl_nl")) {
             item.setIcon(R.drawable.netherlands);
         }
 
